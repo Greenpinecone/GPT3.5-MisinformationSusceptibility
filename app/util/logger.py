797a16276171a8f5e7e6ae705cde7021f164025a
Matthäus Krammer <matthauskrammer@gmail.com>
2024-03-06 16:01:47 +0100

    #1 Added types and reformatted docs of logger class.

--- app/util/logger.py
+++ app/util/logger.py
@@ -3,13 +3,15 @@ This module sets up logging for the application, providing a standardized way
 to log messages and errors across different modules.
 """
 
+from typing import Any
 import logging
 from logging.handlers import RotatingFileHandler
 import streamlit as st
 
 
 class Logger:
-    """Sets up and manages logging for the application.
+    """
+    Sets up and manages logging for the application.
 
     This class configures application-wide logging which can output to both
     console and file. It supports log rotation to prevent log files from
@@ -19,76 +21,69 @@ class Logger:
         logger (logging.Logger): Configured logger instance.
     """
 
-    def __init__(self, name="global", log_file='app.log', level=logging.INFO):
-        """Initializes the logger with specified name, log file, and level.
+    def __init__(self, name: str = "global", log_file: str = 'app.log', level: int = logging.INFO) -> None:
+        """
+        Initializes the logger with specified name, log file, and level.
 
         Args:
-            name (str): Name of the logger, typically __name__ to reflect the
-                module name. Defaults to __name__.
-            log_file (str): Name of the file where logs will be written. 
-                Defaults to 'app.log'.
-            level (int): Logging level, e.g., logging.INFO, logging.DEBUG.
-                Defaults to logging.INFO.
+            name (str): Name of the logger, typically __name__ to reflect the module name. Defaults to 'global'.
+            log_file (str): Name of the file where logs will be written. Defaults to 'app.log'.
+            level (int): Logging level, e.g., logging.INFO, logging.DEBUG. Defaults to logging.INFO.
         """
-        self.logger = logging.getLogger(name)
+        self.logger: logging.Logger = logging.getLogger(name)
 
-        if not self.logger.handlers:  # Check if the logger already has handlers
+        self.logger.setLevel(level)
 
-            self.logger.setLevel(level)
+        # Create formatter
+        formatter: logging.Formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-            # Create formatter
-            formatter = logging.Formatter(
-                '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        # Create console handler and set level to debug
+        ch: logging.StreamHandler = logging.StreamHandler()
+        ch.setLevel(level)
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
 
-            # Create console handler and set level to debug
-            ch = logging.StreamHandler()
-            ch.setLevel(level)
-            ch.setFormatter(formatter)
-            self.logger.addHandler(ch)
+        # Create file handler for logging to a file
+        fh: RotatingFileHandler = RotatingFileHandler(
+            log_file, maxBytes=1048576, backupCount=1)
+        fh.setLevel(level)
+        fh.setFormatter(formatter)
+        self.logger.addHandler(fh)
 
-            # Create file handler for logging to a file
-            # 1MB per file, with backup up to 5 files.
-            fh = RotatingFileHandler(log_file, maxBytes=1048576, backupCount=1)
-            fh.setLevel(level)
-            fh.setFormatter(formatter)
-            self.logger.addHandler(fh)
-
-        # This is a Streamlit-specific setting. Streamlit writes its logs to stdout, and adding a StreamHandler without
-        # checking if the logger already has handlers can result in duplicate logs if Streamlit's re-run mechanism triggers.
-        # The above setup ensures custom logs are also outputted correctly while avoiding duplication.
-
-    def get_logger(self):
-        """Returns the configured logger instance.
+    def get_logger(self) -> logging.Logger:
+        """
+        Returns the configured logger instance.
 
         Returns:
             logging.Logger: The configured logger.
         """
         return self.logger
 
-    def info(self, message):
+    def info(self, message: str) -> None:
         """
         Logs an informational message.
 
         Args:
-            message: The message to be logged.
+            message (str): The message to be logged.
         """
         self.logger.info(message)
 
-    def warning(self, message):
+    def warning(self, message: str) -> None:
         """
         Logs a warning message.
 
         Args:
-            message: The message to be logged.
+            message (str): The message to be logged.
         """
         self.logger.warning(message)
 
-    def error(self, message):
+    def error(self, message: str) -> None:
         """
         Logs an error message.
 
         Args:
-            message: The message to be logged.
+            message (str): The message to be logged.
         """
         self.logger.error(message)
 
@@ -98,29 +93,29 @@ class StreamlitLogger(Logger):
     Extends Logger to add functionality for logging messages to the Streamlit UI.
     """
 
-    def ui_info(self, message):
+    def ui_info(self, message: str) -> None:
         """
         Displays an informational message in the Streamlit UI.
 
         Args:
-            message: The message to be logged.
+            message (str): The message to be displayed.
         """
         st.info(message)
 
-    def ui_warning(self, message):
+    def ui_warning(self, message: str) -> None:
         """
         Displays a warning message in the Streamlit UI.
 
         Args:
-            message: The message to be logged.
+            message (str): The message to be displayed.
         """
         st.warning(message)
 
-    def ui_error(self, message):
+    def ui_error(self, message: str) -> None:
         """
         Displays an error message in the Streamlit UI.
 
         Args:
-            message: The message to be logged.
+            message (str): The message to be displayed.
         """
         st.error(message)
