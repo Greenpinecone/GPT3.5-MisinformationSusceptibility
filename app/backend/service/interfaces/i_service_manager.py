c7ecbd1a37ce6375a64d68e0720c09225a4e37dd
Greenpinecone <100843809+Greenpinecone@users.noreply.github.com>
2024-03-22 18:52:33 +0100

    #39 Added service_manager_facade.py. Added i_service_manager.py.
    1. Created ServiceManagerFacade as single point of access for all separate service classes.
    2. Added the interface IServiceManager for interface structured approach.

---
+++ app/backend/service/interfaces/i_service_manager.py
@@ -0,0 +1,10 @@
+""" This class provides an interface for all service layer implementations 
+"""
+
+from abc import ABC, abstractmethod
+
+
+class IServiceManager(ABC):
+    pass
+
+# TODO: Add service manager functions
