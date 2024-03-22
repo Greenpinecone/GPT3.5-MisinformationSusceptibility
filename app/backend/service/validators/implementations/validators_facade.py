f6ce7e505120650f20931393038e041d36fe4ccd
Greenpinecone <100843809+Greenpinecone@users.noreply.github.com>
2024-03-22 18:21:45 +0100

    #37 Created validators_facade.py to encapsualte all request validation classes.

---
+++ app/backend/service/validators/implementations/validators_facade.py
@@ -0,0 +1,62 @@
+from typing import Dict
+from dataclasses import asdict
+from ....persistence.interfaces.i_data_manager import IDataManager
+from ..classes.request_dto_validators import *
+from ....dtos.create_request import *
+from ....dtos.get_request import *
+
+
+class ValidatorFacade:
+    def __init__(self, data_manager: IDataManager):
+        self.data_manager: IDataManager = data_manager
+        # Initialize each validator with the data_manager
+        self.create_project_validator: CreateProjectSchema = CreateProjectSchema(
+            data_manager=self.data_manager)
+        self.create_dataset_validator: CreateDatasetSchema = CreateDatasetSchema(
+            data_manager=self.data_manager)
+        self.create_datapoint_validator: CreateDataPointSchema = CreateDataPointSchema(
+            data_manager=self.data_manager)
+        self.create_model_validator: CreateModelSchema = CreateModelSchema(
+            data_manager=self.data_manager)
+        self.create_model_evaluation_validator: CreateModelEvaluationSchema = CreateModelEvaluationSchema(
+            data_manager=self.data_manager)
+        self.create_training_run_validator: CreateTrainingRunSchema = CreateTrainingRunSchema(
+            data_manager=self.data_manager)
+        self.get_projects_validator: GetProjectsSchema = GetProjectsSchema(
+            data_manager=self.data_manager)
+        self.get_models_by_project_id_validator: GetModelsByProjectIdSchema = GetModelsByProjectIdSchema(
+            data_manager=self.data_manager)
+        self.get_datasets_by_model_id_validator: GetDatasetsByModelIdSchema = GetDatasetsByModelIdSchema(
+            data_manager=self.data_manager)
+        self.get_datapoints_by_dataset_id_validator: GetDatapointsByDatasetIdSchema = GetDatapointsByDatasetIdSchema(
+            data_manager=self.data_manager)
+
+    def validate_create_project(self, data: CreateProjectDTO) -> Dict:
+        return self.create_project_validator.load(asdict(data))
+
+    def validate_create_dataset(self, data: CreateDatasetDTO) -> Dict:
+        return self.create_dataset_validator.load(asdict(data))
+
+    def validate_create_datapoint(self, data: CreateDataPointDTO) -> Dict:
+        return self.create_datapoint_validator.load(asdict(data))
+
+    def validate_create_model(self, data: CreateModelDTO) -> Dict:
+        return self.create_model_validator.load(asdict(data))
+
+    def validate_create_model_evaluation(self, data: CreateModelEvaluationDTO) -> Dict:
+        return self.create_model_evaluation_validator.load(asdict(data))
+
+    def validate_create_training_run(self, data: CreateTrainingRunDTO) -> Dict:
+        return self.create_training_run_validator.load(asdict(data))
+
+    def prepare_get_projects(self, data: GetProjectsDTO) -> Dict:
+        return self.get_projects_validator.load(asdict(data))
+
+    def prepare_get_models_by_project_id(self, data: GetModelsByProjectIdDTO) -> Dict:
+        return self.get_models_by_project_id_validator.load(asdict(data))
+
+    def prepare_get_datasets_by_model_id(self, data: GetDatasetsByModelIdDTO) -> Dict:
+        return self.get_datasets_by_model_id_validator.load(asdict(data))
+
+    def prepare_get_datapoints_by_dataset_id(self, data: GetDatapointsByDatasetIdDTO) -> Dict:
+        return self.get_datapoints_by_dataset_id_validator.load(asdict(data))
