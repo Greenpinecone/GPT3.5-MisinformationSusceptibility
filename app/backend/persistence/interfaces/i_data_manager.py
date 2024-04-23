0c952af8c5b536852f5cb9f235b2108bf359b004
Greenpinecone <100843809+Greenpinecone@users.noreply.github.com>
2024-04-23 17:36:44 +0200

    #9 Updated types and return value, all DAO functions now return DTOs.
    1. Added mapper to DAO to avoid larger session management - therefore directly converting entities to DTOs in the persistence layer (can be changed later on if necessary).

--- app/backend/persistence/interfaces/i_data_manager.py
+++ app/backend/persistence/interfaces/i_data_manager.py
@@ -2,10 +2,11 @@
 """
 
 from abc import ABC, abstractmethod
-from typing import List, Dict, Any
+from typing import Dict, Any
 from ...database.schema import Project, Dataset, DataPoint, Model, ModelEvaluation, TrainingRun
 from ...dtos.create_request import *
 from ...dtos.get_request import *
+from ...dtos.response import *
 
 
 class IDataManager(ABC):
@@ -16,86 +17,86 @@ class IDataManager(ABC):
         pass
 
     @abstractmethod
-    def save_projects(self, projects_data: List[CreateProjectDTO]) -> List[Project]:
+    def save_projects(self, projects_data: list[CreateProjectDTO]) -> list[ProjectDTO]:
         """Saves or updates project data."""
         pass
 
     @abstractmethod
-    def save_datasets(self, datasets_data: List[CreateDatasetDTO]) -> List[Dataset]:
+    def save_datasets(self, datasets_data: list[CreateDatasetDTO]) -> list[DatasetDTO]:
         """Saves or updates dataset data."""
         pass
 
     @abstractmethod
-    def save_datapoints(self, datapoints_data: List[CreateDataPointDTO]) -> List[DataPoint]:
+    def save_datapoints(self, datapoints_data: list[CreateDataPointDTO]) -> list[DataPointDTO]:
         """Saves or updates datapoint data."""
         pass
 
     @abstractmethod
-    def save_models(self, models_data: List[CreateModelDTO]) -> List[Model]:
+    def save_models(self, models_data: list[CreateModelDTO]) -> list[ModelDTO]:
         """Saves or updates model data."""
         pass
 
     @abstractmethod
-    def save_model_evaluations(self, evaluations_data: List[CreateModelEvaluationDTO]) -> List[ModelEvaluation]:
+    def save_model_evaluations(self, evaluations_data: list[CreateModelEvaluationDTO]) -> list[ModelEvaluationDTO]:
         """Saves or updates model evaluation data."""
         pass
 
     @abstractmethod
-    def save_training_runs(self, runs_data: List[CreateTrainingRunDTO]) -> List[TrainingRun]:
+    def save_training_runs(self, runs_data: list[CreateTrainingRunDTO]) -> list[TrainingRunDTO]:
         """Saves or updates training run data."""
         pass
 
     @abstractmethod
-    def get_all_projects(self, project_data: GetProjectsDTO) -> List[Project]:
+    def get_all_projects(self, project_data: GetProjectsDTO) -> list[ProjectDTO]:
         """Retrieves all projects filterable by name and creation date."""
         pass
 
     @abstractmethod
-    def get_models_by_project_id(self, model_project_data: GetModelsByProjectIdDTO) -> List[Model]:
+    def get_models_by_project_id(self, model_project_data: GetModelsByProjectIdDTO) -> list[ModelDTO]:
         """Retrieves all models associated with a project, filterable by name and version."""
         pass
 
     @abstractmethod
-    def get_datasets_by_model_id(self, dataset_model_data: GetDatasetsByModelIdDTO) -> List[Dataset]:
+    def get_datasets_by_model_id(self, dataset_model_data: GetDatasetsByModelIdDTO) -> list[DatasetDTO]:
         """Retrieves all datasets associated with a model, filterable by dataset name, augmented, and dataset category."""
         pass
 
     @abstractmethod
-    def get_datapoints_by_dataset_id(self, dataset_datapoints_data: GetDatapointsByDatasetIdDTO) -> List[DataPoint]:
+    def get_datapoints_by_dataset_id(self, dataset_datapoints_data: GetDatapointsByDatasetIdDTO) -> list[DataPointDTO]:
         """Retrieves dataset specific datapoints, filterable by coherence score, relevance score, semantic similarity score, and augmentation type."""
         pass
 
     @abstractmethod
-    def get_model_evaluations_by_model_id(self, model_id: int) -> List[ModelEvaluation]:
+    def get_model_evaluations_by_model_id(self, model_id: int) -> list[ModelEvaluationDTO]:
         """Retrieves all model evaluations for a given model."""
         pass
 
     @abstractmethod
-    def get_training_run_by_model_id(self, model_id: int) -> List[TrainingRun]:
+    def get_training_run_by_model_id(self, model_id: int) -> list[TrainingRunDTO]:
         """Retrieves the training run of a model."""
         pass
 
     @abstractmethod
-    def get_model_by_id(self, model_id: int) -> List[Model]:
+    def get_model_by_id(self, model_id: int) -> list[ModelDTO]:
         pass
 
     @abstractmethod
-    def get_dataset_by_id(self, dataset_id: int) -> List[Dataset]:
+    def get_dataset_by_id(self, dataset_id: int) -> list[DatasetDTO]:
         pass
 
     @abstractmethod
-    def get_project_by_id(self, project_id: int) -> List[Project]:
+    def get_project_by_id(self, project_id: int) -> list[ProjectDTO]:
         pass
 
     @abstractmethod
-    def get_training_run_by_id(self, training_run_id: int) -> List[TrainingRun]:
+    def get_training_run_by_id(self, training_run_id: int) -> list[TrainingRunDTO]:
         pass
 
     @abstractmethod
-    def get_datapoint_by_id(self, datapoint_id: int) -> List[DataPoint]:
+    def get_datapoint_by_id(self, datapoint_id: int) -> list[DataPointDTO]:
         pass
 
     @abstractmethod
-    def get_model_evaluation_by_id(self, model_evlauation_id: int) -> list[DataPoint]:
+    def get_model_evaluation_by_id(self, model_evlauation_id: int) -> list[DataPointDTO]:
         pass
     # Implement additional methods as needed based on your DataManager class
