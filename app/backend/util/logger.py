c28145ec7aac39d450ea72986e6c40f32e96386f
Matthäus Krammer <matthauskrammer@gmail.com>
2024-05-06 17:22:53 +0200

    #1 Fixed path creation bug for log file and added CustomValidationError.
    1. Implemented logging context manager for Improved error handling and display.

--- app/backend/util/logger.py
+++ app/backend/util/logger.py
@@ -3,13 +3,13 @@ This module sets up logging for the application, providing a standardized way
 to log messages and errors across different modules.
 """
 
-from typing import Any
 import logging
 from logging.handlers import RotatingFileHandler
 import streamlit as st
 from sqlalchemy.exc import MultipleResultsFound, NoResultFound, SQLAlchemyError
 from marshmallow import ValidationError
-import os
+from ..custom_types.exceptions import CustomValidationError
+from pathlib import Path
 
 
 class Logger:
@@ -24,18 +24,23 @@ class Logger:
         logger (logging.Logger): Configured logger instance.
     """
 
-    def __init__(self, name: str, log_dir: str = 'backend/logs', log_file: str = 'app.log', level: int = logging.DEBUG) -> None:
+    def __init__(self, name: str, log_dir: str = 'backend/logs', log_file: str = 'app.log', root_marker: str = "app", level: int = logging.DEBUG) -> None:
         """
         Configures a logger with the given name, log level, and log file.
         This method ensures that each logger is only configured once.
         """
 
-        # Ensure the log directory path is correctly formed
-        log_path = os.path.join(log_dir, log_file)
+        # Get the path to the directory where this script runs
+        current_path = Path(__file__).resolve()
 
-        # Ensure the directory exists (not the file path)
-        log_directory = os.path.dirname(log_path)
-        os.makedirs(log_directory, exist_ok=True)
+        # Find the project root
+        project_root = self.find_project_root(current_path, root_marker)
+
+        # Construct the full path to the log file
+        log_path = project_root / log_dir / log_file
+
+        # Ensure the log directory exists
+        log_path.parent.mkdir(parents=True, exist_ok=True)
 
         self.logger = logging.getLogger(name)
         if not self.logger.handlers:  # Check if the logger already has handlers
@@ -56,6 +61,17 @@ class Logger:
             fh.setFormatter(formatter)
             self.logger.addHandler(fh)
 
+    def find_project_root(self, start_path: Path, root_marker: str) -> Path:
+        """
+        Traverse up from start_path to find the directory marked by a marker.
+        """
+        for parent in start_path.parents:
+            # Check if the parent directory or a marker file exists
+            if (parent / root_marker).exists() or (parent.name == root_marker):
+                return parent
+        raise FileNotFoundError(
+            f"Project root not found starting from {start_path}")
+
     def __getattr__(self, name):
         """
         Forward attribute access to the underlying logging.Logger object.
@@ -69,9 +85,10 @@ class StreamlitLogger(Logger):
     Extends Logger to add functionality for logging messages to the Streamlit UI.
     """
 
-    def __init__(self, name: str, log_dir: str = 'backend/logs', log_file: str = 'app.log', level: int = logging.DEBUG) -> None:
+    def __init__(self, name: str, errors_container: st.container, log_dir: str = 'backend/logs', log_file: str = 'app.log', root_marker: str = "app", level: int = logging.DEBUG) -> None:
+        self.errors_container = errors_container
         # Call the parent class's __init__ method
-        super().__init__(name, log_dir, log_file, level)
+        super().__init__(name, log_dir, log_file, root_marker, level)
 
     def __getattr__(self, name):
         """
@@ -80,6 +97,77 @@ class StreamlitLogger(Logger):
         the underlying logging.Logger object if not found.
         """
         return super().__getattr__(name)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        # We need to check if exc_type is an instance of Exception to avoid catching "non error" exception which are directly derived from the BaseException class like streamlits "RerunException" (or KeyboardInterrupt) to avoid issues with page reload / page reloads after navigating to another page, when using the context manager for error handling.
+        if exc_type is not None and issubclass(exc_type, Exception):
+            self.handle_exception(exc_type, exc_value)
+            # Suppresses the exception unless it needs to propagate.
+            return True
+        else:
+            # Lets BaseExceptions propagate furhter to avoid streamlit page reload issues.
+            return False
+
+    def handle_exception(self, exc_type, exc_value):
+        with self.errors_container:
+            if issubclass(exc_type, NoResultFound):
+                self.ui_error(
+                    f"Check the database an inconsistency might have occurred:\n{exc_value}")
+            elif issubclass(exc_type, MultipleResultsFound):
+                self.ui_error(
+                    f"Check the database, an id duplication might have occurred:\n{exc_value}")
+            elif issubclass(exc_type, ValidationError):
+                self.ui_error(
+                    f"An unchecked validation error has been thrown:\n{exc_value}")
+            elif issubclass(exc_type, CustomValidationError):
+                self.handle_validation_error(exc_value)
+            elif issubclass(exc_type, SQLAlchemyError):
+                self.ui_error(
+                    f"Something went wrong with the database:\n{exc_value}")
+            elif issubclass(exc_type, Exception):
+                self.ui_error(f"An unexpected error occurred:\n{exc_value}")
+
+    def handle_validation_error(self, e):
+        print(e, e.errors, e.operation_type)
+        if hasattr(e, 'errors') and hasattr(e, 'operation_type'):
+            # Handles dictionaries with lists, single values or None.
+            error_messages = self._process_errors(e.errors)
+            message = e.message if e.message else "Validation errors occurred during operaion"
+            self.ui_error(f"{message} '{e.operation_type}':\n" +
+                          "\n".join(error_messages))
+        else:
+            self.ui_error(f"Validation errors occurred:\n{e}")
+
+    def _format_error_message(self, idx, field, msgs):
+        if isinstance(msgs, dict):
+            # Format messages from a dictionary
+            return f"{idx+1}. {field}: {', '.join(f'{key}={value}' for key, value in msgs.items())}"
+        elif isinstance(msgs, list):
+            # Join list items into a single string
+            return f"{idx+1}. {field}: {', '.join(msgs)}"
+        else:
+            # Handle other cases or malformed messages
+            return f"{idx+1}. {field}: (Invalid message format)"
+
+    def _process_errors(self, errors):
+        if isinstance(errors, dict):
+            # Process errors assuming it is a dictionary of messages
+            return [self._format_error_message(idx, field, msgs) for idx, (field, msgs) in enumerate(errors.items())]
+        elif isinstance(errors, list):
+            # Process errors assuming it is a list of generic error messages
+            return [f"{idx+1}. Error: {error}" for idx, error in enumerate(errors)]
+        else:
+            # Return a generic error if the format is unexpected
+            return ["Unexpected error format"]
+
+    def ui_error(self, message: str) -> None:
+        """
+        Displays an error message in the Streamlit UI.
+        """
+        st.error(message)
 
     def ui_info(self, message: str) -> None:
         """
@@ -107,19 +195,3 @@ class StreamlitLogger(Logger):
             message (str): The message to be displayed.
         """
         st.error(message)
-
-    def display_errors(self, func, *args, **kwargs):
-        try:
-            return func(*args, **kwargs)  # Or handle successful result
-        except NoResultFound as e:
-            self.ui_error(
-                str(f"Check the database an inconsistency might have occured: {e}"))
-        except MultipleResultsFound as e:
-            self.ui_error(
-                str(f"Check the database, an id duplication might have occured: {e}"))
-        except ValidationError as e:
-            self.ui_error(str(e))
-        except SQLAlchemyError as e:
-            self.ui_error(str(f"Something went wrong with the database: {e}"))
-        except Exception as e:
-            self.ui_error("An unexpected error occurred, check the logs.")
