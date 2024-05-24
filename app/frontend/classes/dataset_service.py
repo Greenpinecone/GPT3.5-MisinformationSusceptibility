a79da78ceedebe2d10f7f9eb7320c6aec464835b
Matthäus Krammer <matthauskrammer@gmail.com>
2024-05-24 19:04:36 +0200

    #50 #51 #52 Added a dataset service class, a paginator class for pagination and a toast manager class for global toast display management.

---
+++ app/frontend/classes/dataset_service.py
@@ -0,0 +1,79 @@
+from typing import Any
+import streamlit as st
+from app.backend.service.implementations.service_manager_facade import ServiceManagerFacade
+from frontend.classes.dataset_editor import DatasetEditor
+from backend.dtos.create_request import CreateDatasetDTO, CreateDataPointDTO
+from frontend.mappers.frontend_mappers import ConvertDataPointDTOWithDataFrameWrapperToCreateDatapointDTO
+from backend.database.schema import DatasetCategory, MessageKeys
+from backend.util import utility_functions as uf
+
+
+class DatasetService:
+    @staticmethod
+    def process_and_create_dataset(all_dataset_editors: list[DatasetEditor], is_global: bool, dataset_name: str, project_id: int, service: ServiceManagerFacade) -> None:
+        if not dataset_name:
+            uf.show_toast("Dataset name is required.", "info")
+            return
+
+        all_dataset_dtos = []
+        all_datapoint_dtos = []
+
+        for dataset_editor in all_dataset_editors:
+            # Update categories column if not a training dataset, since the user couldhave submitted before reloading the test datset after changes in the training dataset.
+            if dataset_editor.dataset_category == DatasetCategory.test:
+                DatasetService.update_test_dataset_categories(dataset_editor)
+
+            datapoint_dtos = ConvertDataPointDTOWithDataFrameWrapperToCreateDatapointDTO(
+                many=True).dump(dataset_editor.datapoints)
+            dataset_dto = DatasetService.create_dataset_dto(
+                dataset_name, dataset_editor, project_id, is_global)
+
+            all_dataset_dtos.append(dataset_dto)
+            all_datapoint_dtos.append(datapoint_dtos)
+
+        DatasetService.submit_all_datasets_and_datapoints(
+            all_dataset_dtos, all_datapoint_dtos, service)
+
+    @staticmethod
+    def create_dataset_dto(dataset_name: str, dataset_editor: DatasetEditor, project_id: int, is_global: bool) -> CreateDatasetDTO:
+        return CreateDatasetDTO(
+            dataset_name=dataset_name,
+            category=DatasetCategory(
+                dataset_editor.dataset_category) if dataset_editor.dataset_category else None,
+            augmented=False,
+            fine_tuning_company=dataset_editor.chosen_company,
+            fine_tuning_formatting=dataset_editor.chosen_file_format,
+            fine_tuning_model=dataset_editor.chosen_model,
+            project_ids=[project_id],
+            is_global=is_global
+        )
+
+    @staticmethod
+    def submit_all_datasets_and_datapoints(dataset_dtos: list[CreateDatasetDTO], datapoint_dtos: list[list[CreateDataPointDTO]], service: ServiceManagerFacade):
+        service.create_dataset_with_datapoints(
+            dataset_dtos[0], datapoint_dtos[0], dataset_dtos[1], datapoint_dtos[1])
+        uf.show_toast("Dataset has been successfully created.", "success")
+
+    @staticmethod
+    def update_test_dataset_categories(dataset_editor: DatasetEditor):
+        for datapoint in dataset_editor.datapoints:
+            dataset_editor.dataframe_editor.set_default_category_if_not_in_list(
+                datapoint.messages, dataset_editor.shared_category_tracker)
+
+    def update_dataset_editor_if_changed(attr: str, new_value: Any, all_dataset_editors: list[DatasetEditor]) -> bool:
+        changed = False
+        for editor in all_dataset_editors:
+            if getattr(editor, attr) != new_value:
+                setattr(editor, attr, new_value)
+                # Reset temp dialog dataset editor
+                editor.temp_simple_datapoint_dto = None
+                # Reset default role
+                editor.default_role = MessageKeys[editor.chosen_company.value].value[
+                    0][0] if editor.chosen_company else None
+                changed = True
+        return changed
+
+    @staticmethod
+    def check_if_dataset_editor_has_datapoints(all_dataset_editors: list[DatasetEditor]) -> bool:
+        """Checks if any dataset editor has datapoints."""
+        return any(len(dataset_editor.datapoints) > 0 for dataset_editor in all_dataset_editors)
---
+++ app/frontend/classes/paginator.py
@@ -0,0 +1,31 @@
+import math
+import streamlit as st
+from typing import Any
+from streamlit.delta_generator import DeltaGenerator
+from uuid import uuid4
+
+
+class Paginator:
+    def __init__(self, items_per_page: int = 50):
+        self.items_per_page = items_per_page
+        self.current_page = 1
+
+    def get_paginated_items(self, items: list[Any]) -> list[Any]:
+        start_index = (self.current_page - 1) * self.items_per_page
+        end_index = start_index + self.items_per_page
+        return items[start_index:end_index]
+
+    def create_pagination_buttons(self, pagination_buttons_container: DeltaGenerator, items: list[Any]):
+        num_items = len(items)
+        total_pages = math.ceil(num_items / self.items_per_page)
+        buttons_per_row = 10
+
+        for row_start in range(1, total_pages + 1, buttons_per_row):
+            row_end = min(row_start + buttons_per_row, total_pages + 1)
+            with pagination_buttons_container:
+                cols = st.columns(min(buttons_per_row, row_end - row_start))
+                for page_num, col in zip(range(row_start, row_end), cols):
+                    with col:
+                        if st.button(label=str(page_num), key=f"page_{page_num}_{uuid4()}"):
+                            self.current_page = page_num
+                            st.experimental_rerun()
