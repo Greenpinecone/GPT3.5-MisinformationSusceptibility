22322c7e917089e5d4b2ba2e18e3e78d80e671cb
Matthäus Krammer <matthauskrammer@gmail.com>
2024-05-31 15:19:19 +0200

    #52 Updated ToastManager to use config GLOBAL_SESSION_STATE_KEYS instead of removed global_toasts.

--- app/frontend/classes/toast_manager.py
+++ app/frontend/classes/toast_manager.py
@@ -1,26 +1,35 @@
 import streamlit as st
 from app.frontend.dataclasses.dataclasses import ToastMessage
-from backend.util.global_states import global_toasts
+from backend.util.config import GLOBAL_SESSION_STATE_KEYS
 
 
 class ToastManager:
-    @classmethod
-    def add_global_toasts(cls, message: str, message_type: str = 'info'):
-        """Add a toast message to the global toasts."""
-        icon = cls.get_icon_for_message_type(message_type)
-        global_toasts.append(ToastMessage(message, icon))
 
     @staticmethod
-    def show_global_toasts():
-        """Show all toasts from the global state."""
-        if global_toasts:
-            for toast in global_toasts:
-                st.toast(body=toast.message, icon=toast.icon)
-            # Clear toasts after showing
-            global_toasts.clear()
+    def _initialize_global_toasts() -> None:
+        """Ensure the global toasts session state is initialized."""
+        if not st.session_state.get(GLOBAL_SESSION_STATE_KEYS['GLOBAL_TOASTS_KEY']):
+            st.session_state[GLOBAL_SESSION_STATE_KEYS['GLOBAL_TOASTS_KEY']] = []
 
     @classmethod
-    def show_toast(cls, message: str, message_type: str = 'info'):
+    def add_global_toasts(cls, message: str, message_type: str = 'info') -> None:
+        """Add a toast message to the global toasts."""
+        cls._initialize_global_toasts()
+        icon = cls.get_icon_for_message_type(message_type)
+        st.session_state[GLOBAL_SESSION_STATE_KEYS['GLOBAL_TOASTS_KEY']].append(
+            ToastMessage(message, icon))
+
+    @staticmethod
+    def show_global_toasts() -> None:
+        """Show all toasts from the global state."""
+        if GLOBAL_SESSION_STATE_KEYS['GLOBAL_TOASTS_KEY'] in st.session_state and st.session_state[GLOBAL_SESSION_STATE_KEYS['GLOBAL_TOASTS_KEY']]:
+            for toast in st.session_state[GLOBAL_SESSION_STATE_KEYS['GLOBAL_TOASTS_KEY']]:
+                st.toast(body=toast.message, icon=toast.icon)
+            # Clear toasts after showing
+            st.session_state[GLOBAL_SESSION_STATE_KEYS['GLOBAL_TOASTS_KEY']].clear()
+
+    @classmethod
+    def show_toast(cls, message: str, message_type: str = 'info') -> None:
         """Show a toast."""
         icon = cls.get_icon_for_message_type(message_type)
         st.toast(message, icon)
