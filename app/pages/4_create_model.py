f6f80bb884ff48965cc73eefee3437df951c0f56
Matthäus Krammer <matthauskrammer@gmail.com>
2024-06-03 15:06:51 +0200

    #43 Added model creation functionality.

--- app/pages/4_create_model.py
+++ app/pages/4_create_model.py
@@ -16,11 +16,17 @@ from frontend.classes.query_params_manager import QueryParamsManager
 from frontend.classes.page_navigator import PageNavigator
 from app.frontend.classes.global_app_state_manager import GlobalAppStateManager
 from frontend.util import utility_functions as frontend_uf
+from backend.util.config import DTO_LIST_FORMATTING_PRESETS as formattings
 
 
 apply_global_style()
 errors_container = st.container()
 logger: StreamlitLogger = StreamlitLogger(__name__, errors_container)
+
+
+def all_fields_set(model_name: str, selected_training_dataset: DatasetDTO) -> bool:
+    return model_name and selected_training_dataset
+
 
 with logger:
 
@@ -43,46 +49,53 @@ with logger:
             def switch_to_create_model_interface():
                 model_name = st.text_input(label="Enter the name of your model", max_chars=255,
                                            placeholder="The name of your model...", key="model_name_input", label_visibility="hidden")
-                search_cols = st.columns(2)
-                with search_cols[0]:
 
-                    training_datasets: list[DatasetDTO] = service.filter_datasets(GetDatasetsDTO(
-                        category=DatasetCategory.training, project_id=current_project.id))
+                training_datasets: list[DatasetDTO] = service.filter_datasets(GetDatasetsDTO(
+                    category=DatasetCategory.training, project_id=current_project.id))
 
-                    selected_training_datasets = st.selectbox(label="Select a training dataset", options=training_datasets, index=None, key="training_dataset_selectbox",
-                                                              label_visibility="hidden" if training_datasets else "visible", placeholder="Choose a training dataset" if training_datasets else "No options available")
+                selected_training_dataset = st.selectbox(label="Select a training dataset", options=training_datasets, index=None, key="training_dataset_selectbox",
+                                                         label_visibility="collapsed" if training_datasets else "visible", placeholder="Choose a training dataset" if training_datasets else "No options available", format_func=lambda dto: frontend_uf.display_dto(dto, formattings["DATASETDTO_SIMPLE"]))
 
-                with search_cols[1]:
+                corresponding_test_dataset = None
+                if selected_training_dataset:
+                    corresponding_test_dataset = service.get_dataset_by_id(
+                        selected_training_dataset.test_dataset_id)[0]
 
-                    test_datasets: list[DatasetDTO] = service.filter_datasets(GetDatasetsDTO(
-                        category=DatasetCategory.test, project_id=current_project.id))
+                st.markdown("###### Corresponding test dataset:")
+                if corresponding_test_dataset:
+                    with st.container(border=True):
+                        st.write(frontend_uf.display_dto(
+                            corresponding_test_dataset, formattings["DATASETDTO_SIMPLE"]) or None)
+                else:
+                    None
 
-                    selected_test_dataset = st.selectbox(label="Select a test dataset", options=test_datasets, index=None, key="test_dataset_selectbox",
-                                                         label_visibility="hidden" if test_datasets else "visible", placeholder="Choose a training dataset" if test_datasets else "No options available")
-            switch_to_create_model_interface()
+                is_global = st.checkbox(label="Make the model globally available", value=False, key="globalize_model_checkbox",
+                                        help="Globalized models can be selected in the list of available models when creating a new project", label_visibility="visible")
 
-            frontend_uf.create_text_divider("or")
+                frontend_uf.create_text_divider("or")
 
-            @st.experimental_fragment
-            def switch_to_create_dataset_interface():
                 create_dataset_button = st.button("Create Dataset", help="Click me to create a new dataset",
                                                   type="secondary", key="create_dataset")
                 if create_dataset_button:
                     GlobalAppStateManager.clear_session_state_except()
                     PageNavigator.navigate_to_page('create_dataset')
 
-            switch_to_create_dataset_interface()
+                frontend_uf.create_text_divider()
 
-            frontend_uf.create_text_divider()
-
-            @st.experimental_fragment
-            def create_model():
                 create_model_button = st.button(label="Create Model",
-                                                key="create_model_button", type="primary")
+                                                key="create_model_button", type="primary", disabled=not all_fields_set(model_name, selected_training_dataset))
                 if create_model_button:
-                    pass
-                # TODO: Implement model saving
+                    create_model_dto = CreateModelDTO(model_name=model_name, project_ids=[
+                                                      current_project.id], training_dataset_id=selected_training_dataset.id, is_global=is_global)
 
-            create_model()
+                    created_model_dto: ModelDTO = service.create_model(
+                        create_model_dto)
+                    if created_model_dto:
+                        GlobalAppStateManager.clear_session_state_except()
+                        ToastManager.add_global_toasts(
+                            "Model has been successfully saved.", "success")
+                        PageNavigator.navigate_to_page("fine_tune_model")
+
+            switch_to_create_model_interface()
 
     load_page()
