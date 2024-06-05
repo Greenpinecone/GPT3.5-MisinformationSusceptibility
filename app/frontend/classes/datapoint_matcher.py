6f040c148d43a6377c6d347ffe46132b7262e6ed
Matthäus Krammer <matthauskrammer@gmail.com>
2024-06-05 15:39:05 +0200

    #61 Implemented DataPointMatcher class.

---
+++ app/frontend/classes/datapoint_matcher.py
@@ -0,0 +1,62 @@
+from io import BytesIO
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly as pl
+from app.frontend.classes.dataframe_widget_provider import DataFrameWidgetProvider
+from app.frontend.classes.global_app_state_manager import GlobalAppStateManager
+from backend.util.logger import StreamlitLogger
+from backend.service.implementations.service_manager_facade import ServiceManagerFacade
+from backend.dtos.get_request import *
+from backend.dtos.response import *
+from backend.dtos.create_request import *
+from backend.database.schema import DatasetCategory, MessageKeys, UploadFormats
+from app.frontend.classes.file_uploader import FileUploader
+from app.frontend.classes.dataframe_editor import DataFrameEditor
+from uuid import uuid4
+from frontend.dtos.frontend_dtos import DataPointDTOWithDataFrameWrapper, MessagesContainer
+from streamlit.delta_generator import DeltaGenerator
+from frontend.classes.paginator import Paginator
+from frontend.classes.toast_manager import ToastManager
+from streamlit.components.v1 import html
+
+
+class DataPointMatcher:
+    def __init__(self, training_datapoints_dtos: list[DataPointDTO], test_datapoint_dtos: list[DataPointDTO]):
+        self.training_datapoints = training_datapoints_dtos
+        self.test_datapoints = test_datapoint_dtos
+        self.current_test_datapoint = None
+
+    def set_current_datapoint(self, current_datapoint: int):
+        self.current_test_datapoint = current_datapoint
+
+    def load(self):
+        trainings_dataset_paginator: Paginator = GlobalAppStateManager.get_or_create_session_state(
+            "training_paginator", default_value=Paginator, items_per_page=5
+        )
+        test_dataset_paginator: Paginator = GlobalAppStateManager.get_or_create_session_state(
+            "test_paginator", default_value=Paginator, items_per_page=1
+        )
+
+        test_pagination_buttons_container = st.container()
+        paginated_test_datapoints = test_dataset_paginator.get_paginated_items(
+            self.test_datapoints)
+        test_dataset_paginator.create_pagination_buttons(
+            test_pagination_buttons_container, self.test_datapoints)
+
+        st.markdown("###### Test Datapoint")
+        for test_datapoint in paginated_test_datapoints:
+            self.current_test_datapoint = test_datapoint
+            DataFrameWidgetProvider.create_simple_dataframe(
+                test_datapoint)
+
+        training_pagination_buttons_container = st.container()
+        paginated_trainings_datapoints = trainings_dataset_paginator.get_paginated_items(
+            self.training_datapoints)
+        trainings_dataset_paginator.create_pagination_buttons(
+            training_pagination_buttons_container, self.training_datapoints)
+
+        st.markdown("###### Trainings Datapoints")
+        for trainings_datapoint in paginated_trainings_datapoints:
+            DataFrameWidgetProvider.create_dataframe_with_checkbox(
+                trainings_datapoint, self.current_test_datapoint)
