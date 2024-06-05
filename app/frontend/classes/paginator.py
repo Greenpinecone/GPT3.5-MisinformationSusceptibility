1789b560b2ddde22fa976f6f7572ef457090e810
Greenpinecone <100843809+Greenpinecone@users.noreply.github.com>
2024-06-05 15:29:19 +0200

    #51 #54 Updated page navigator to make it more adaptable and made paginator UI more user friendly..

--- app/frontend/classes/paginator.py
+++ app/frontend/classes/paginator.py
@@ -1,35 +1,59 @@
-import math
 import streamlit as st
-from typing import Any
 from streamlit.delta_generator import DeltaGenerator
 from uuid import uuid4
+import math
 
 
 class Paginator:
     def __init__(self, items_per_page: int = 50) -> None:
         self.items_per_page = items_per_page
         self.current_page = 1
+        self.total_pages = 1
 
-    def get_paginated_items(self, items: list[Any]) -> list[Any]:
+    def get_total_pages(self, num_items: int) -> int:
+        return math.ceil(num_items / self.items_per_page)
+
+    def get_paginated_items(self, items: list) -> list:
         start_index = (self.current_page - 1) * self.items_per_page
         end_index = start_index + self.items_per_page
         return items[start_index:end_index]
 
-    def create_pagination_buttons(self, pagination_buttons_container: DeltaGenerator, items: list[Any]) -> None:
-        num_items = len(items)
-        total_pages = math.ceil(num_items / self.items_per_page)
-        buttons_per_row = 10
-
-        for row_start in range(1, total_pages + 1, buttons_per_row):
-            row_end = min(row_start + buttons_per_row, total_pages + 1)
-            with pagination_buttons_container:
-                cols = st.columns(buttons_per_row)
-                for page_num, col in zip(range(row_start, row_end), cols):
-                    with col:
-                        st.button(label=str(page_num), key=f"""page_{page_num}_{
-                                  uuid4()}""", on_click=lambda page=page_num: self.set_page_num(page))
-
     def set_page_num(self, page_num: int):
         self.current_page = page_num
-        print(self.current_page)
-        print("HALLO")
+
+    def create_pagination_buttons(self, pagination_buttons_container: DeltaGenerator, items: list, columns_partition: list[float] = [3, 1, 1.5, 1, 3]) -> None:
+        num_items = len(items)
+        self.total_pages = self.get_total_pages(num_items)
+
+        if self.total_pages > 1:
+            with pagination_buttons_container:
+                st.write(f"Page {self.current_page} of {self.total_pages}")
+
+                # Adjust column widths as needed
+                cols = st.columns(columns_partition)
+                with cols[1]:
+                    if self.current_page > 1:
+                        prev_button_key = f"""prev_{
+                            uuid4()}"""
+                        st.button(label="Prev", key=prev_button_key, help="s", on_click=lambda: self.set_page_num(
+                            self.current_page - 1))
+
+                with cols[2]:
+                    page_input_key = f"page_input_{uuid4()}"
+                    st.number_input(
+                        label="Current page",
+                        min_value=1,
+                        max_value=self.total_pages,
+                        value=self.current_page,
+                        key=page_input_key,
+                        label_visibility="collapsed",
+                        on_change=lambda: self.set_page_num(
+                            st.session_state[page_input_key]
+                        ))
+
+                with cols[3]:
+                    if self.current_page < self.total_pages:
+                        next_button_key = f"""next_{
+                            uuid4()}"""
+                        st.button(label="Next", key=next_button_key, on_click=lambda: self.set_page_num(
+                            self.current_page + 1))
