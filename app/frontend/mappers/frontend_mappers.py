3eea012761edf2c27bb7f6d313bdba8743a6f2c0
Matthäus Krammer <matthauskrammer@gmail.com>
2024-06-11 17:56:19 +0200

    #36 #57 Updated frontend mapper to changed database schema, frontend util function and added preliminary scroll_to_bottom implementation.

--- app/frontend/mappers/frontend_mappers.py
+++ app/frontend/mappers/frontend_mappers.py
@@ -12,12 +12,6 @@ class ConvertDataPointDTOWithDataFrameWrapperToCreateDatapointDTO(Schema):
         serialize=lambda obj: obj.datapoint_dto.dataset_id if obj.datapoint_dto else None)
     related_datapoint_ids = fields.Function(
         serialize=lambda obj: obj.datapoint_dto.related_datapoint_ids if obj.datapoint_dto else None)
-    coherence_score = fields.Function(
-        serialize=lambda obj: obj.datapoint_dto.coherence_score if obj.datapoint_dto else None)
-    relevance_score = fields.Function(
-        serialize=lambda obj: obj.datapoint_dto.relevance_score if obj.datapoint_dto else None)
-    semantic_similarity_score = fields.Function(
-        serialize=lambda obj: obj.datapoint_dto.semantic_similarity_score if obj.datapoint_dto else None)
     augmentation_type = fields.Function(
         serialize=lambda obj: obj.datapoint_dto.augmentation_type if obj.datapoint_dto else None)
     initial_datapoint_id = fields.Function(
@@ -32,12 +26,6 @@ class DataPointDTOToUpdateDataPointDTO(Schema):
     id = fields.Function(serialize=lambda obj: obj.id)
     related_datapoint_ids = fields.Function(
         serialize=lambda obj: [datapoint.id for datapoint in obj.related_datapoints])
-    coherence_score = fields.Function(
-        serialize=lambda obj: obj.coherence_score)
-    relevance_score = fields.Function(
-        serialize=lambda obj: obj.relevance_score)
-    semantic_similarity_score = fields.Function(
-        serialize=lambda obj: obj.semantic_similarity_score)
 
     @post_dump
     def make_update_datapoint_dto(self, data, **kwargs):
