af0fb91ae97d8131e09274f7377585e6b655b2d0
Matthäus Krammer <matthauskrammer@gmail.com>
2024-07-05 18:03:43 +0200

    #6 Refactor | Add: Modularized SemanticSimilarityCalculator functions - New calculate_model_evaluations_semantic_similarity_score function.

--- app/backend/service/classes/evaluation/semantic_similarity_calculator.py
+++ app/backend/service/classes/evaluation/semantic_similarity_calculator.py
@@ -1,5 +1,6 @@
 from sentence_transformers import SentenceTransformer, models, util, SimilarityFunction
-from app.backend.dtos.create_request import CreateDataPointDTO, CreateDataPointEvaluationDTO
+from app.backend.database.schema import ModelEvaluation
+from app.backend.dtos.create_request import CreateDataPointDTO, CreateDataPointEvaluationDTO, CreateModelEvaluationDTO
 from app.backend.dtos.response import DataPointDTO, DataPointEvaluationDTO
 import torch
 
@@ -102,12 +103,33 @@ class SemanticSimilarityCalculator:
             semantic_similarity_model["model_name"], similarity_fn_name=SimilarityFunction.COSINE)
         return model
 
+    @staticmethod
+    def calculate_semantic_similarity_score_for_text_pair(text_1: str, text_2: str, model: SentenceTransformer):
+        # Compute embeddings using the model
+        # Uses numpy arrays by default
+        original_embedding = model.encode(
+            text_1)
+        augmented_embedding = model.encode(
+            text_2)
+
+        # Calculate cosine similarity
+        # Uses PyTorch or NumPy depending on if a tensor or numpy array is passed. PyTorch could theoretically be used to laverage CPU AND GPU.
+        return model.similarity(
+            original_embedding, augmented_embedding)
+
+    @staticmethod
+    def calculate_average_similarity_and_scale_to_percent(similarities: list[torch.Tensor]):
+        # Calculate average similarity and convert to float
+        avg_similarity = torch.tensor(similarities).mean().item()
+        # Scale from -1 to 1 to 0 to 100
+        return (avg_similarity + 1) * 50
+
     @classmethod
     def calculate_datapoints_semantic_similarity_score(cls,
-                                                       datapoints: list[CreateDataPointDTO],
+                                                       datapoints: list[DataPointDTO],
                                                        augmented_datapoints: list[CreateDataPointDTO], datapoint_evaluations: list[CreateDataPointEvaluationDTO],
                                                        semantic_similarity_model: dict
-                                                       ) -> tuple[list[CreateDataPointDTO], list[CreateDataPointEvaluationDTO]]:
+                                                       ) -> list[CreateDataPointEvaluationDTO]:
 
         model: SentenceTransformer = cls.initialize_model(
             semantic_similarity_model)
@@ -134,24 +156,35 @@ class SemanticSimilarityCalculator:
                 original_content = original_message['content']
                 augmented_content = augmented_message['content']
 
-                # Compute embeddings using the model
-                # Uses numpy arrays by default
-                original_embedding = model.encode(
-                    original_content)
-                augmented_embedding = model.encode(
-                    augmented_content)
-
-                # Calculate cosine similarity
-                # Uses PyTorch or NumPy depending on if a tensor or numpy array is passed. PyTorch could theoretically be used to laverage CPU AND GPU.
-                similarity = model.similarity(
-                    original_embedding, augmented_embedding)
+                similarity: torch.Tensor = cls.calculate_semantic_similarity_score_for_text_pair(
+                    original_content, augmented_content, model)
                 similarities.append(similarity.item())
 
-            # Calculate average similarity and convert to float
-            avg_similarity = torch.tensor(similarities).mean().item()
-            # Scale from -1 to 1 to 0 to 100
-            scaled_similarity = (avg_similarity + 1) * 50
+            scaled_similarity: float = cls.calculate_average_similarity_and_scale_to_percent(
+                similarities)
             # Assign similarity score to datapoint evaluation dto
             evaluation_dto.semantic_similarity_score = scaled_similarity
 
-        return augmented_datapoints, datapoint_evaluations
+        return datapoint_evaluations
+
+    @classmethod
+    def calculate_model_evaluations_semantic_similarity_score(cls, model_evaluations: list[CreateModelEvaluationDTO], test_datapoints: list[DataPointDTO], semantic_similarity_model: dict) -> list[CreateModelEvaluationDTO]:
+        model: SentenceTransformer = cls.initialize_model(
+            semantic_similarity_model)
+
+        for test_datapoint, model_evaluation in zip(test_datapoints, model_evaluations):
+            # Fetches the last message of the test datapoint and the last message of the model evaluation (which has been complete by the current model) and compares their semantic similarity.
+            # TODO: Could be overhauled by adding a separate messages table where the user can on upload of datapoints deinfe which rows to remove during the test run for the model to know what to re-complete for comparison in model evaluation.
+            datapoints_last_message_content = test_datapoint.messages["messages"][-1]['content']
+            model_evaluations_last_message_content = model_evaluation.messages[
+                "messages"][-1]['content']
+
+            similarity: torch.Tensor = cls.calculate_semantic_similarity_score_for_text_pair(
+                datapoints_last_message_content, model_evaluations_last_message_content, model)
+
+            scaled_similarity: float = cls.calculate_average_similarity_and_scale_to_percent([
+                                                                                             similarity])
+
+            model_evaluation.semantic_similarity_score = scaled_similarity
+
+        return model_evaluations
