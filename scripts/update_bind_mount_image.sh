4014caec4f60d6bfa5ee2085022764bbca97f390
Greenpinecone <100843809+Greenpinecone@users.noreply.github.com>
2025-02-23 21:33:14 +0100

    #34 UPDATE: Updated config files.
    1. Added *.db files to dockerignore to avoid them being included in the public image.
    2. Set the default theme of the application to "dark".
    3. Updated bind mount script to correct naming, forcing no-cache on execution and added start command with private API keys and removed unnecessary code.

--- scripts/update_bind_mount_image.sh
+++ scripts/update_bind_mount_image.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set variables for image and container names
-IMAGE_NAME="bachelor_work_project_image"
-CONTAINER_NAME="bachelor_work_project_container"
+IMAGE_NAME="gpt-3.5-misinformation-image"
+CONTAINER_NAME="gpt-3.5-misinformation-container"
 
 # Path in the container where the current directory will be mounted
 MOUNT_PATH="/app"
@@ -14,12 +14,21 @@ PORT_MAPPING="8501:8501"
 echo "Removing existing container named $CONTAINER_NAME..."
 docker rm -f $CONTAINER_NAME
 
-# Build the Docker image (this assumes your Dockerfile is in the current directory)
+# Build the Docker image (this assumes your Dockerfile is in the current directory) + forcing no cache
 echo "Building Docker image $IMAGE_NAME..."
-docker build -t $IMAGE_NAME .
+docker build --no-cache -t $IMAGE_NAME .
 
+echo "Docker image built successfully!"
+
+# ONLY RUN THIS CODE IF YOU WANT YOUR PRIVATE DATA TO BE INCLUDED IN THE DOCKER IMAGE - PRIVATE USE
 # Run the container with the specified configurations / Only Unix / Linux
-echo "Running new container named $CONTAINER_NAME from image $IMAGE_NAME..."
-docker run -d -v $(pwd):$MOUNT_PATH --name $CONTAINER_NAME -p $PORT_MAPPING $IMAGE_NAME
+# echo "Running new container named $CONTAINER_NAME from image $IMAGE_NAME..."
+# docker run -d \
+#   --env-file $(pwd)/.env \  # Load environment variables from .env file
+#   -v $(pwd):$MOUNT_PATH \  # Mount current directory to /app in the container
+#   -v $(pwd)/google_auth_key/gpt_streamlit_misinformation_auth_key.json:/app/google_auth_key/gpt_streamlit_misinformation_auth_key.json \  # Mount the auth key file
+#   --name $CONTAINER_NAME \  # Assign a name to the container
+#   -p $PORT_MAPPING \  # Map port 8501 from host to container
+#   $IMAGE_NAME
 
-echo "Container $CONTAINER_NAME is now running."
+# echo "Container $CONTAINER_NAME is now running."
